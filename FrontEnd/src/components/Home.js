0390b7d0277d87ea33d2aa6337677514a901408f
sidd023 <sidd>
2020-08-07 23:29:27 +1000

    HOME PAGE
    HomePAge with different user login done

---
+++ FrontEnd/src/components/Home.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+export default function Home() {
+    return (
+        <Fragment>
+            <a className=" coolBeans body-hov" href="/">ADMIN</a>
+            <a className=" coolBeans body-hov" href="/">EMPLOYEE</a>
+            <a className=" coolBeans body-hov" href="/">USER</a>
+        </Fragment >
+    )
+}
