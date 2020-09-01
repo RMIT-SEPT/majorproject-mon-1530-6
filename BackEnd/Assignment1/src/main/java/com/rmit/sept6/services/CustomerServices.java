df45585e92684dec97af0402c63ac53739c432ec
s3677729 <s3677729@student.rmit.edu.au>
2020-09-01 16:30:50 +1000

    Customer Startup

---
+++ BackEnd/Assignment1/src/main/java/com/rmit/sept6/services/CustomerServices.java
@@ -0,0 +1,10 @@
+package com.rmit.sept6.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerServices {
+	@Autowired
+	private CustomerRepository customerRepository
+}
